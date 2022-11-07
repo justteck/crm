afa5dfc73c3c2426308d347879db440fd99e81c4
justteck <justteck@yandex.ru>
2022-11-07 14:26:23 +0300

    lesson_02

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+console.log('modalTitle: ', modalTitle);
+
+const modalForm = document.querySelector('.modal__form');
+console.log('modalForm: ', modalForm);
+
+const modalIsDiscountCheckbox = document.querySelector('.modal__checkbox');
+console.log('modalIsDiscountCheckbox: ', modalIsDiscountCheckbox);
+
+const modalDiscountCount = document.querySelector('.modal__input_discount');
+console.log('modalDiscountCount: ', modalDiscountCount);
